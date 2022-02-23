0c09d00070ca1a6429b14f2cd442abbcb78b2f9f
Himakar Raju <himakarraju47@gmail.com>
2022-02-23 07:06:27 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { Navbar,NavbarBrand } from 'reactstrap';
-import './App.css';
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
+import "./App.css";
 
-import React from 'react'
+import React from "react";
 
 function App() {
-  return (
-    <div className = "App" >
-      <Navbar dark color = "primary" >
-        <div className = "container" >
-          <NavbarBrand href="/" > Ristorante Con Fusion </NavbarBrand>
-        </div>
-      </Navbar> 
-    </div >
-  )
+	return (
+		<div className="App">
+			<Navbar dark color="primary">
+				<div className="container">
+					<NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
+				</div>
+			</Navbar>
+			<Menu />
+		</div>
+	);
 }
 
-export default App
+export default App;
